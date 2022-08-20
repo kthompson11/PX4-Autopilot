aa989f7a4a2b5298f44bc6a265b0ae84a3074d1a
Kenneth Thompson <ken.thompson141@gmail.com>
2022-08-20 13:55:37 -0700

    arm_state_estimator: Add copter arm state estimator

---
+++ src/modules/arm_state_estimator/ArmStateEstimator.cpp
@@ -0,0 +1,125 @@
+
+#include "ArmStateEstimator.hpp"
+
+ArmStateEstimator::ArmStateEstimator() :
+	ModuleParams(nullptr),
+	ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::hp_default)
+{
+}
+
+ArmStateEstimator::~ArmStateEstimator()
+{
+}
+
+int ArmStateEstimator::init()
+{
+	if (_param_arm_state_ch.get() < 0) {
+		PX4_ERR("The copter arm state ADC channel must be defined.");
+		return PX4_ERROR;
+	}
+
+	_adc_report_sub.registerCallback();
+
+	return PX4_OK;
+}
+
+void ArmStateEstimator::Run()
+{
+	if (should_exit()) {
+		exit_and_cleanup();
+		return;
+	}
+
+	adc_report_s report{};
+
+	if (!_adc_report_sub.update(&report)) {
+		return;
+	}
+
+	// TODO: kalman filter
+	const float theta = calculate_sensed_arm_angle(report);
+	float angle_radians = _filter.apply(theta);
+
+	arm_state_s state{};
+	state.timestamp = hrt_absolute_time();
+	state.angle = angle_radians;
+	_arm_state_pub.publish(state);
+}
+
+float ArmStateEstimator::calculate_sensed_arm_angle(const adc_report_s &report)
+{
+	float angle = NAN;
+
+	// Look up the index of the chosen ADC channel
+	if (_adc_sensor_index < 0) {
+		for (unsigned i = 0; i < arraySize(adc_report_s::channel_id); ++i) {
+			if (report.channel_id[i] == _param_arm_state_ch.get()) {
+				_adc_sensor_index = i;
+			}
+		}
+	}
+
+	// Calculate the arm angle
+	if (_adc_sensor_index >= 0) {
+		const int32_t adc_counts = report.raw_data[_adc_sensor_index];
+		const float voltage = report.v_ref * adc_counts / report.resolution;
+
+		const float offset = _param_arm_state_offset.get();
+		const float scale = _param_arm_state_scale.get();
+		angle = (voltage - offset) * scale;
+	}
+
+	return angle;
+}
+
+int ArmStateEstimator::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int ArmStateEstimator::task_spawn(int argc, char *argv[])
+{
+	ArmStateEstimator *instance = new ArmStateEstimator();
+
+	if (instance) {
+		_object.store(instance);
+		_task_id = task_id_is_work_queue;
+
+		if (instance->init() == PX4_OK) {
+			return PX4_OK;
+		}
+
+	} else {
+		PX4_ERR("alloc failed");
+	}
+
+	delete instance;
+	_object.store(nullptr);
+	_task_id = -1;
+
+	return PX4_ERROR;
+}
+
+int ArmStateEstimator::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s\n", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+copter arm state estimator
+
+)DESCR_STR");
+
+	PRINT_MODULE_USAGE_COMMAND("start");
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+
+	return 0;
+}
+
+extern "C" __EXPORT int arm_state_estimator_main(int argc, char *argv[])
+{
+	return ArmStateEstimator::main(argc, argv);
+}
---
+++ src/modules/arm_state_estimator/ArmStateEstimator.hpp
@@ -0,0 +1,59 @@
+
+#include <inttypes.h>
+#include <stdint.h>
+
+#include <drivers/drv_hrt.h>
+#include <lib/mathlib/math/filter/MedianFilter.hpp>
+#include <lib/perf/perf_counter.h>
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/px4_config.h>
+
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+
+#include <uORB/Publication.hpp>
+#include <uORB/topics/arm_state.h>
+
+#include <uORB/Subscription.hpp>
+#include <uORB/SubscriptionCallback.hpp>
+#include <uORB/topics/adc_report.h>
+
+using namespace time_literals;
+
+class ArmStateEstimator : public ModuleBase<ArmStateEstimator>, public ModuleParams, public px4::ScheduledWorkItem
+{
+public:
+	ArmStateEstimator();
+
+	~ArmStateEstimator() override;
+
+	/** @see ModuleBase */
+	static int task_spawn(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int custom_command(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int print_usage(const char *reason = nullptr);
+
+	int init();
+
+private:
+	void Run() override;
+
+	float calculate_sensed_arm_angle(const adc_report_s &report);
+
+	int _adc_sensor_index{-1};
+	math::MedianFilter<float, 3> _filter;
+
+	uORB::Publication<arm_state_s> _arm_state_pub{ORB_ID(arm_state)};
+
+	uORB::SubscriptionCallbackWorkItem _adc_report_sub{this, ORB_ID(adc_report)};
+
+	DEFINE_PARAMETERS(
+		(ParamInt<px4::params::ARM_STATE_CH>) _param_arm_state_ch,
+		(ParamFloat<px4::params::ARM_STATE_OFFSET>) _param_arm_state_offset,
+		(ParamFloat<px4::params::ARM_STATE_SCALE>) _param_arm_state_scale
+	)
+};
